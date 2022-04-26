a6fcf1eeda922e1bfa34de46a82bda1a99328add
Marculonis21 <markbecvar@gmail.com>
2022-04-26 12:54:04 +0200

    SineWaveHalf agent starting to work well

--- gaAgent.py
+++ gaAgent.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from abc import ABC, abstractmethod
 import numpy as np
+import random
+import math
 
 import GAMethods
 GA = GAMethods.GA()
@@ -51,3 +53,86 @@ class StepCycleHalfAgent(AgentType):
 
     def mutation(self, population):
         return GA.mutation(population, self.action_size//2, indiv_mutation_prob=0.25, action_mutation_prob=0.03)
+
+
+class SineFuncHalfAgent(AgentType):
+    # individual = amplitude, frequency, shift-x, shift-y for each leg
+    def get_action(self, individual, step):
+        actions = []
+        for i in range(len(individual)//4):
+            amp    = individual[4*i]
+            freq   = individual[4*i+1]
+            shiftx = individual[4*i+2]
+            shifty = individual[4*i+3]
+
+            value = amp*math.sin(freq*(step/10) + shiftx) + shifty
+            if value > 1:
+                value = 1
+            if value < -1:
+                value = -1
+
+            actions.append(value)
+
+        full_action = np.array([ actions[0], actions[1], actions[2], actions[3],
+                                -actions[0],-actions[1],-actions[2],-actions[3]])
+        # full_action = np.array([ actions[0], actions[1], actions[2], actions[3],
+        #                          actions[4], actions[5], actions[6], actions[7]])
+
+        return np.array(full_action)
+
+    def generate_population(self, population_size):
+        population = []
+
+        for _ in range(population_size):
+            individual = []
+            for i in range(4):
+                # individual = [[random.uniform(0,1), random.uniform(0,10), random.uniform(0, 2*math.pi)] for _ in range(4)]
+                individual.append(random.uniform(0.2,1))         # amplitude
+                individual.append(random.uniform(0.5,10))        # frequency
+                individual.append(random.uniform(0,2*math.pi)) # shift-x
+                individual.append(random.uniform(-0.5,0.5))        # shift-y
+
+            population.append(individual)
+
+        return population
+
+    def selection(self, population, fitness_values):
+        return GA.tournament_selection(population, fitness_values, 5)
+
+    def crossover(self, population):
+        return GA.crossover_uniform(population)
+
+    def mutation(self, population):
+        new_population = []
+
+        individual_mutation_rate = 0.75
+        gene_mutation_rate = 0.10
+
+        for individual in population:
+            if random.random() < individual_mutation_rate:
+                category = -1
+                for i in range(len(individual)):
+                    category = (category + 1) % 4
+                    if random.random() < gene_mutation_rate:
+                        if category == 0:
+                            individual[i] = random.uniform(0.2,1)
+                        if category == 1:
+                            individual[i] = random.uniform(0.5,10)
+                        if category == 2:
+                            individual[i] = random.uniform(0, 2*math.pi)
+                        if category == 3:
+                            individual[i] = random.uniform(-0.5,0.5)
+
+
+            new_population.append(individual)
+
+        return new_population
+
+if __name__ == "__main__":
+    agent = SineFuncHalfAgent()
+    indiv = agent.generate_population(1)[0]
+
+    print(indiv)
+    for i in range(10):
+        print(i, agent.get_action(indiv, i))
+

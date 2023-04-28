8c8b692e72dfe14a59a750df21c0fa8bf90d0d25
Marculonis21 <markbecvar@gmail.com>
2023-04-29 01:06:18 +0200

    new experiments, finished LARGE experiment

--- experiment_setter.py
+++ experiment_setter.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 import typing
-import re
 import time
+import sys
+import copy
 
 import resources.gaAgents as gaAgents
 import resources.robots.robots as robots
@@ -19,15 +20,16 @@ class Experiments:
     dictionary with selected custom name.
     """
 
-    __batch_dir = ".saves/batch_runs/@1_run_@2_@3_@4/"
+    __batch_dir = "./saves/batch_runs/@1_run_@2_@3_@4/"
     __experiments : typing.Dict[str, RunParams]
 
     def __init__(self):
         self.__experiments = {}
-        self.__experiments["exp11_TFS"] = self.exp11_TFS_spotlike()
-        self.__experiments["exp12_TFS"] = self.exp12_TFS_spotlike()
-        self.__experiments["exp11_SineFull"] = self.exp11_SineFull_spotlike()
-        self.__experiments["exp12_SineFull"] = self.exp12_SineFull_spotlike()
+        self.__experiments["exp10_TFS"] = self.exp10_TFS_spotlike(False)
+        self.__experiments["exp11_TFS"] = self.exp11_TFS_spotlike(False)
+        self.__experiments["exp12_TFS"] = self.exp12_TFS_spotlike(False)
+        self.__experiments["exp11_SineFull"] = self.exp11_SineFull_spotlike(False)
+        self.__experiments["exp12_SineFull"] = self.exp12_SineFull_spotlike(False)
 
     def __create_batch_dir(self, robot, agent, note):
         batch_dir = self.__batch_dir.replace("@1", note).replace(
@@ -36,14 +38,38 @@ class Experiments:
                                              "@4", str(time.time()))
         return batch_dir
 
+    def __exp_start_note(self):
+        print("Starting experiment:", sys._getframe(1).f_code.co_name)
+
     def get_experiment_names(self):
         return list(self.__experiments.keys());
 
     def get_experiment(self, name):
         assert name in self.get_experiment_names(), f"Unknown experiment name - list of created experiments {self.get_experiment_names()}"
-        return self.__experiments[name]
+        return copy.copy(self.__experiments[name])
 
-    def exp11_TFS_spotlike(self):
+    def exp10_TFS_spotlike(self, run=True):
+        robot = robots.SpotLike()
+        agent = gaAgents.TFSAgent(robot, [False]*len(robot.body_parts))
+        note = "exp1.0"
+
+        batch_dir = self.__create_batch_dir(robot, agent, note)
+
+        params = RunParams(robot, 
+                           agent,
+                           ga_population_size=100,
+                           ga_generation_count=500,
+                           show_best=False,
+                           save_best=True,
+                           save_dir=batch_dir,
+                           note="")
+
+        if run: # print note before starting experiment
+            self.__exp_start_note()
+
+        return params
+
+    def exp11_TFS_spotlike(self, run=True):
         robot = robots.SpotLike()
         agent = gaAgents.TFSAgent(robot, [False]*len(robot.body_parts))
         note = "exp1.1"
@@ -59,9 +85,12 @@ class Experiments:
                            save_dir=batch_dir,
                            note="")
 
+        if run: # print note before starting experiment
+            self.__exp_start_note()
+
         return params
 
-    def exp12_TFS_spotlike(self):
+    def exp12_TFS_spotlike(self, run=True):
         robot = robots.SpotLike()
         agent = gaAgents.TFSAgent(robot, [False]*len(robot.body_parts))
         note = "exp1.2"
@@ -77,9 +106,12 @@ class Experiments:
                            save_dir=batch_dir,
                            note="")
 
+        if run: # print note before starting experiment
+            self.__exp_start_note()
+
         return params
 
-    def exp11_SineFull_spotlike(self):
+    def exp11_SineFull_spotlike(self, run=True):
         robot = robots.SpotLike()
         agent = gaAgents.SineFuncFullAgent(robot, [False]*len(robot.body_parts))
         note = "exp1.1"
@@ -95,9 +127,12 @@ class Experiments:
                            save_dir=batch_dir,
                            note="")
 
+        if run: # print note before starting experiment
+            self.__exp_start_note()
+
         return params
 
-    def exp12_SineFull_spotlike(self):
+    def exp12_SineFull_spotlike(self, run=True):
         robot = robots.SpotLike()
         agent = gaAgents.SineFuncFullAgent(robot, [False]*len(robot.body_parts))
         note = "exp1.2"
@@ -112,5 +147,8 @@ class Experiments:
                            save_best=True,
                            save_dir=batch_dir,
                            note="")
+
+        if run: # print note before starting experiment
+            self.__exp_start_note()
 
         return params
